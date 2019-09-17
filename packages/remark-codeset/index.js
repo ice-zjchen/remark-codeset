df2e60ab42f96e5f34aa8637f45d3158b0bcba23
ice-zjchen <ice.zjchen@gmail.com>
2019-09-17 15:56:55 +0800

    fix(remark-codeset) add default parser

--- packages/remark-codeset/index.js
+++ packages/remark-codeset/index.js
@@ -1,7 +1,8 @@
 const {tokenize, parse} = require('remark-codeset-core');
+const markdown = require('remark-parse');
 
 module.exports = (options) => {
-    const parser = options.parser;
+    const parser = options.parser || markdown;
 
     return (tree, file) => {
         tokenize({tree, parser});
